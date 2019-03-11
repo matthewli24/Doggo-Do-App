c5422e93a821075a1522361a319280c7b0478b10
Matthew Li <matthewli2491@gmail.com>
2019-03-11 14:58:22 -0400

    adjust css

--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -3,13 +3,13 @@ import Dog from '../assets/dog.svg'
 import Dog1 from '../assets/dog1.svg'
 
 const Jumbotron = ({username, accessToken}) => {
-  const name = (username && accessToken) ? (<div>Welcome {username}</div>) : null
+  const name = (username && accessToken) ? (<h1>Welcome {username}</h1>) : null
     
   return (
     <div className="jumbotron jumbotron-fluid bg-primary text-white text-center">
       <div className="d-flex flex-row justify-content-around">
         <img src={Dog1}/>
-        <div className="display-4"> Doggo-Do App </div>
+        <div className="display-3"> Doggo-Do App </div>
         <img src={Dog}/>
       </div>
       {name}
