c3449dd99c4ea571c1e5e69db208de6f3d9dc633
Matthew Li <matthewli2491@gmail.com>
2019-03-12 12:29:20 -0400

    fix jumbotron

--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Dog from '../assets/dog.svg'
 import Dog1 from '../assets/dog1.svg'
+import './iconImgFormat.css'
 
 const Jumbotron = ({username, accessToken}) => {
   const name = (username && accessToken) ? (<h1>Welcome {username}</h1>) : null
@@ -8,9 +9,9 @@ const Jumbotron = ({username, accessToken}) => {
   return (
     <div className="jumbotron jumbotron-fluid bg-primary text-white text-center">
       <div className="d-flex flex-row justify-content-around">
-        <img src={Dog1}/>
+        <img className="jumboImgFormat" src={Dog1}/>
         <div className="display-3"> Doggo-Do App </div>
-        <img src={Dog}/>
+        <img className="jumboImgFormat" src={Dog}/>
       </div>
       {name}
     </div>
